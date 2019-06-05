2764e51227f9eb87ffd0423628ae300f003bc378
arisaghafari <arisa_ghafari@outlook.com>
2019-06-05 18:37:08 +0430

    axa bala nemiyan

--- kalapet/kalapet/urls.py
+++ kalapet/kalapet/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('forosh.urls')),
 
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG: # new
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
